56a8a50b16fbc64e62c4f213969d3be3e9018931
Nazar356 <k.nazar.og@gmail.com>
2023-04-17 19:22:40 +0300

    [AX-7] - Calendly :: Track attribution for bookings without UTM tags

--- src/js/calendly.js
+++ src/js/calendly.js
@@ -1,9 +1,12 @@
 jQuery(function ($) {
   let calendlyUrlUpdated = 0;
 
-  $(document).on("attx.updated attx.no_params", function (e) { //Wait until storage will be updated.
+  /**
+   * Wait until storage will be updated.
+   */
+  $(document).on("attx.updated", function () {
 
-    window.addEventListener("message", (e) => { 
+    window.addEventListener("message", (e) => {
 
       //Wait for the iframe to load
       //to get access to iframe URL.
@@ -55,6 +58,58 @@ jQuery(function ($) {
 
         }
 
+      }
+
+    }, false);
+
+  });
+
+
+  /**
+   * Save default attributions if we don't have params in the URL.
+   */
+  $(document).on("attx.no_params", function () {
+
+    window.addEventListener("message", (e) => {
+
+      //Wait for the iframe to load
+      //to get access to iframe URL.
+      if (
+        e.origin === "https://calendly.com" &&
+        e.data.event &&
+        e.data.event.indexOf("calendly.") === 0
+      ) {
+
+        let attributions = {};
+
+        attributions = addDefaultParams(attributions);
+
+        //Check if we already updated iframe URL 
+        //to prevent infinite iframe reloading.
+        if (calendlyUrlUpdated) {
+          return false;
+        }
+
+        let newUrl = '';
+        let oldUrl = jQuery("iframe[src*='calendly.com/']").attr("src");
+
+        newUrl = oldUrl;
+
+        if (attributions.hasOwnProperty("ref")) {
+          newUrl = updateQueryStringParameter(newUrl, "utm_source", attributions.ref);
+        }
+
+        if (attributions.hasOwnProperty("source")) {
+          newUrl = updateQueryStringParameter(newUrl, "utm_medium", attributions.source);
+        }
+
+        if (attributions.hasOwnProperty("path")) {
+          newUrl = updateQueryStringParameter(newUrl, "utm_campaign", attributions.path);
+        }
+
+        //Update iframe URL.
+        jQuery("iframe[src*='calendly.com/']").attr("src", newUrl);
+        calendlyUrlUpdated = 1;
       }
 
     }, false);
