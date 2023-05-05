13af9673424f363f4fd5ecc3bf684865fc7706e0
Nazar356 <k.nazar.og@gmail.com>
2023-05-05 18:28:05 +0300

    [AX-15] - Add a `attx`prefix to every field we need to add.

--- inc/gf-integration.php
+++ inc/gf-integration.php
@@ -18,46 +18,46 @@ function attx_gform_pre_submission_handler($form)
    //Populate fields.
    if (isset($form["fields"]) && is_array($form["fields"])) {
       foreach ($form["fields"] as $field) {
-         if ($field["inputName"] === "visitor_ids") {
+         if ($field["inputName"] === "attx_visitor_ids") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["visitorIds"]) ? json_encode($storage["visitorIds"]) : "";
-         } else if ($field["inputName"] === "utm_id") {
+         } else if ($field["inputName"] === "attx_utm_id") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["utm_id"]) ? $storage["attribution"]["utm_id"] : "";
-         } else if ($field["inputName"] === "utm_source") {
+         } else if ($field["inputName"] === "attx_utm_source") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["utm_source"]) ? $storage["attribution"]["utm_source"] : "";
-         } else if ($field["inputName"] === "utm_medium") {
+         } else if ($field["inputName"] === "attx_utm_medium") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["utm_medium"]) ? $storage["attribution"]["utm_medium"] : "";
-         } else if ($field["inputName"] === "utm_campaign") {
+         } else if ($field["inputName"] === "attx_utm_campaign") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["utm_campaign"]) ? $storage["attribution"]["utm_campaign"] : "";
-         } else if ($field["inputName"] === "utm_term") {
+         } else if ($field["inputName"] === "attx_utm_term") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["utm_term"]) ? $storage["attribution"]["utm_term"] : "";
-         } else if ($field["inputName"] === "utm_content") {
+         } else if ($field["inputName"] === "attx_utm_content") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["utm_content"]) ? $storage["attribution"]["utm_content"] : "";
-         } else if ($field["inputName"] === "gclid") {
+         } else if ($field["inputName"] === "attx_gclid") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["gclid"]) ? $storage["attribution"]["gclid"] : "";
-         } else if ($field["inputName"] === "fbclid") {
+         } else if ($field["inputName"] === "attx_fbclid") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["fbclid"]) ? $storage["attribution"]["fbclid"] : "";
-         } else if ($field["inputName"] === "tduid") {
+         } else if ($field["inputName"] === "attx_tduid") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["tduid"]) ? $storage["attribution"]["tduid"] : "";
-         } else if ($field["inputName"] === "referer") {
+         } else if ($field["inputName"] === "attx_referer") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["ref"]) ? urldecode($storage["attribution"]["ref"]) : "";
-         } else if ($field["inputName"] === "path") {
+         } else if ($field["inputName"] === "attx_path") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["path"]) ? $storage["attribution"]["path"] : "";
-         } else if ($field["inputName"] === "source") {
+         } else if ($field["inputName"] === "attx_source") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["source"]) ? $storage["attribution"]["source"] : "";
-         } else if ($field["inputName"] === "time") {
+         } else if ($field["inputName"] === "attx_time") {
 
             $_POST['input_' . $field["id"]] =  isset($storage["attribution"]["time"]) ? $storage["attribution"]["time"] : "";
          }
