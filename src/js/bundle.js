191146b6d24866f6a4b17e96b94fe5370c3cc66e
Nazar356 <devprog22@gmail.com>
2024-11-05 21:17:42 +0200

    Add hubspot support

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -251,6 +251,32 @@ jQuery(function ($) {
 })
 jQuery(function ($) {
 
+    /**
+     * Add the hidden field to all HubSpot forms to pass localStorage data.
+     */
+    $(document).on("attx.updated", function (e) {
+        let attxData = JSON.parse(decodeBase64(localStorage.getItem("attx")));
+        let lastStoredAttxData = attxData[attxData.length - 1];
+                    
+        setInterval(function () {
+            let input = "";
+
+            if (lastStoredAttxData) {
+                input = $(document).find(".hs-form-iframe").contents().find("[name='attx_data']");
+
+                if (input.length) {
+                    input[0].value = JSON.stringify(lastStoredAttxData);
+                }
+
+            }
+
+        }, 1000);
+        
+    });
+
+});  
+jQuery(function ($) {
+
   /**
    * Add the hidden field to all quForms to pass localStorage data to the server.
    */
